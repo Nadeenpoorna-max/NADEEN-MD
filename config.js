4ad24b89ec1d431e623bc7b5e680d0ad9ee6632b
Nadeenpoorna-max <nadeenpoorna5@gmail.com>
2024-08-11 22:35:47 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,4 +6,17 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID,
+ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/f2a6ee1ab649a2fb72708.jpg",
+ALIVE_MSG: process.env.ALIVE_MSG || "👨‍💻💝 Hello, I am NADEEN-MD,I am alive Now! 
+> Bot Developed by @NADEEN POORNA
+    
+> contact Bot Owner - +94711451319 
+ 
+> Powered By DINKA FILMS 
+
+🌀 YouTube - www.youtube.com/@DINKA_FILMS
+🌀 Follow WhatsApp Channel - https://whatsapp.com/channel/0029VagN2qW3gvWUBhsjcn3I
+-----------------------------------------------------------
+-----------------------------------------------------------
+ ᴛʜɪꜱ ꜱʏꜱᴛᴇᴍ ᴄʀᴇᴀᴛᴇ ʙʏ ©ɴᴀᴅᴇᴇɴ ᴘᴏᴏʀɴᴀ "   
 };
