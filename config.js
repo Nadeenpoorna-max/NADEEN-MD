2f0401ad6f635f4dcc91af912337fd7e8c2adf68
Nadeenpoorna-max <nadeenpoorna5@gmail.com>
2024-08-17 23:06:56 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,20 +7,20 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
 SESSION_ID: process.env.SESSION_ID,
 ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/f2a6ee1ab649a2fb72708.jpg",
-ALIVE_MSG: process.env.ALIVE_MSG || "👨‍💻💝 Hello, I am NADEEN-MD,I am alive Now! 🇱🇰
+ALIVE_MSG: process.env.ALIVE_MSG || "👨‍💻💝 Hello, I am `NADEEN-MD`,I am alive Now! 🇱🇰
 
-> 👨🏻‍💻 Bot Developed by @NADEEN POORNA   
+> 👨🏻‍💻 Bot Developed by *@NADEEN POORNA*   
 
 > 🥏Contact Bot Owner - +94711451319 
 
-> 🛡 Powered By DINKA FILMS 
+> 🛡 Powered By *DINKA FILMS* 
 ----------------------------------------------------------
-⚙ Type .menu to get the bot's command list. 
+⚙ Type *.menu* to get the bot's command list. 
 ----------------------------------------------------------
 ----------------------------------------------------------
 🌀 YouTube - www.youtube.com/@DINKA_FILMS
 
 🌀 Follow WhatsApp Channel - https://whatsapp.com/channel/0029VagN2qW3gvWUBhsjcn3I
 ----------------------------------------------------------
- ᴛʜɪꜱ ꜱʏꜱᴛᴇᴍ ᴄʀᴇᴀᴛᴇ ʙʏ ©ɴᴀᴅᴇᴇɴ ᴘᴏᴏʀɴᴀ"
+ `ᴛʜɪꜱ ꜱʏꜱᴛᴇᴍ ᴄʀᴇᴀᴛᴇ ʙʏ ©ɴᴀᴅᴇᴇɴ ᴘᴏᴏʀɴᴀ`"
 };
