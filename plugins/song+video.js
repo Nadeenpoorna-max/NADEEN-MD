8df089502736b3693535b29b0093cace0ee65930
Nadeenpoorna-max <nadeenpoorna5@gmail.com>
2024-08-19 18:31:11 +0530

    Update song+video.js

--- plugins/song+video.js
+++ plugins/song+video.js
@@ -1,1 +1,17 @@
+const config = require('../config')
+const {cmd , commands} = require('../command')
 
+cmd({
+    pattern: "song",
+    desc: "download song.",
+    category: "download",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+
+}catch(e){
+  console.log(e)
+  reply(`${e}`) 
+}
+})
