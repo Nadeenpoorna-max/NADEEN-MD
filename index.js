08bcaecedecedcf6b2897c36ebf125d59bae1e4c
Nadeenpoorna-max <nadeenpoorna5@gmail.com>
2024-09-01 09:28:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -69,7 +69,7 @@ require("./plugins/" + plugin);
 console.log('Plugins installed successful ✅')
 console.log('ŋąɖɛɛŋ-🇧​​🇴​​🇹​ connected to whatsapp ✅')
 
-let up = `NADEEN-MD​ connected successful ✅ ` Contact Bot Owner - +94711 451 319` \n\nPREFIX: ${prefix}`;
+let up = '`NADEEN-MD​ connected successful ✅`' \n\nPREFIX: ${prefix}`;
 
 conn.sendMessage(ownerNumber + "@s.whatsapp.net", { image: { url: `https://telegra.ph/file/f5e0751267307ac475187.mp4` }, caption: up })
 
