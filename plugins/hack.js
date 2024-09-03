c56fc855700c3ff86025398fbd47c6d25f7096c1
Nadeenpoorna-max <nadeenpoorna5@gmail.com>
2024-09-03 22:07:45 +0530

    Add files via upload

---
+++ plugins/hack.js
@@ -0,0 +1,117 @@
+const {fetchJson,cmd, tlang,sleep } = require('../lib')
+
+//---------------------------------------------------------------------------
+
+cmd({
+
+            pattern: "hack",
+
+            category: "prank",
+
+            desc: "hacking prank",
+
+            use: ' ',
+
+            filename: __filename,
+
+        },
+
+        async(Void,citel, text) => {
+
+citel.reply("```Injecting malware```")
+
+await sleep(1000)
+
+citel.reply("```hacking into device \n 0%```")
+
+await sleep(1000)
+
+citel.reply("```transfering photos \n █ 10%```")
+
+await sleep(1000)
+
+citel.reply("```transfer successful \n █ █ 20%```")
+
+await sleep(1000)
+
+citel.reply("```transfering videos \n █ █ █ 30%```")
+
+await sleep(1000)
+
+citel.reply("```transfer successful \n █ █ █ █ 40%```")
+
+await sleep(1000)
+
+citel.reply("```transfering audio \n █ █ █ █ █ 50%```")
+
+await sleep(1000)
+
+citel.reply("```transfer successful \n █ █ █ █ █ █ 60%```")
+
+await sleep(1000)
+
+citel.reply("```transfering hidden files \n █ █ █ █ █ █ █ 70%```")
+
+await sleep(1000)
+
+citel.reply("```transfer successful \n █ █ █ █ █ █ █ █ 80%```")
+
+await sleep(1000)
+
+citel.reply("```transfering whatsapp chat \n █ █ █ █ █ █ █ █ █ 90%```")
+
+await sleep(1000)
+
+citel.reply("```transfer successful \n █ █ █ █ █ █ █ █ █ █ 100%```")
+
+await sleep(1000)
+
+citel.reply("```System hyjacking on process.. \n Conecting to Server ```")
+
+await sleep(1000)
+
+citel.reply("```Divice successfully connected... \n Riciving data...```")
+
+await sleep(1000)
+
+citel.reply("```Data hyjacked from divice 100% completed \n killing all evidence killing all malwares...```")
+
+await sleep(1000)
+
+citel.reply("``` HACKING COMPLETED ```")
+
+await sleep(1000)
+
+citel.reply("``` SENDING PHONE DOCUMENTS...```")
+
+await sleep(1000)
+
+citel.reply("``` UPLOADING FILE...```")
+
+await sleep(1000)
+                    
+citel.reply("```<script.type=@whatsapp.net#hackjoson.//http//zkcgfsilhyer083ty3098t 0d9puzpoivhoicAHdpliahndoihd/.gf;lc?.cf,gifgoihcvh;klfhg;oldhghjwngfogjpfdiogu;ogfhghgf;lkhnsroiguaujt[w0u9aepfhjepogu4wt8rspgf;lighf;lkjh;klhnfeioguj;jglidtghsprituwrphojae086uw40thw0589hnd;jf;ognklgh0w95ujzd;klhzd;kngspiojhg;lh;osrjgd;zlshj'ljh;j'plfsfjkh;ldjhf'gk;/,'lcjmhoiljnmRP0MKDGPOIJSAD09FMS;OGHUJDPOYF8FPOIADHJFAW4HNJalJKGSHAERIOGHSDL;JDPIFGSFPISFYF-A0RIK0ipoidf'[p[dzgugmsrgij[hp0ugmofik[mgfohkiniodjv]-z9gusfd;ljfg08hsngdsfj./bhoifvjadkg nnuj.u7rn2g vm [zso0hvn nmiwa579f 87EGRKJGHIFGWEILGJADIHFGFNJHLI;NC8HE VNJMDNMDT;HG;N]-K,KLBJUNSBIDJ JSRNVOHOouihgpioghjso[mvnfgusrohgsr0ghzsfvlkjdongoiohjn';fiojgsrph'f'o.jops<type/>...```")
+
+await sleep(1000)
+                    
+citel.reply("``` SUCCESSFULLY CONNECTED DEVICE```")
+
+await sleep(1000)
+                    
+citel.reply("``` SUCCESSFULLY SENT DATA AND Connection disconnected```")
+
+await sleep(1000)
+                                      
+citel.reply("``` SICURITY CODE = *WE4FR3G*```")
+
+await sleep(1000)
+                    
+citel.reply(" `ᴛʜɪꜱ ꜱʏꜱᴛᴇᴍ ᴄʀᴇᴀᴛᴇ ʙʏ ©ɴᴀᴅᴇᴇɴ ᴘᴏᴏʀɴᴀ`")
+
+await sleep(1000)
+
+            return citel.reply('*ALL FILES TRANSFERRED*');
+
+        }
+
+    )
