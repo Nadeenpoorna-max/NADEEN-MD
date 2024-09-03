c2515d70e11636b46e6eb3dda1a5ea996baefe8e
Nadeenpoorna-max <nadeenpoorna5@gmail.com>
2024-09-03 22:28:36 +0530

    Update owner.js

--- plugins/owner.js
+++ plugins/owner.js
@@ -11,16 +11,16 @@ cmd({
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
 
-let dec = `*🧚‍♀️𝐃𝐀𝐑𝐊 𝐙𝐄𝐑𝐎-𝐌𝐃🧚‍♀️*
+let dec = `*⚡𝙽𝙰𝙳𝙴𝙴𝙽 - 𝙼𝙳⚡*
 
 > *𝗢𝗪𝗡𝗘𝗥 𝗜𝗡𝗙𝗢* 
 
-*⚡ᴏᴡɴᴇʀ ɴᴀᴍᴇ -: ʜᴀᴄᴋᴇʀ ᴛᴍ ᴏꜰᴄ (ᴅᴀʀᴋ ᴢᴇʀᴏ ᴛᴍ)*
-*⚡ɴᴜᴍʙᴇʀ* -: 94757286833
+*⚡ᴏᴡɴᴇʀ ɴᴀᴍᴇ -: ​🇳​​🇦​​🇩​​🇪​​🇪​​🇳​ ​🇵​​🇴​​🇴​​🇷​​🇳​​🇦​ (🇩​​🇮​​🇳​​🇰​​🇦​ ​🇴​​🇼​​🇳​​🇪​​🇷​)*
+*⚡ɴᴜᴍʙᴇʀ* -: 94711451319
 
-*©𝗗ᴀʀᴋ ᴢᴇʀᴏ-ᴍᴅ ʙʏ ᴅᴀʀᴋ ᴢᴇʀᴏ ʜᴀᴄᴋᴇʀ ᴛᴍッ*
+*`👨‍💻 ɴᴀᴅᴇᴇɴ-ᴍᴅ ᴍᴀᴅᴇ ʙʏ ɴᴀᴅᴇᴇɴ ᴘᴏᴏʀɴᴀ 👨‍💻`*
 `
-await conn.sendMessage(from,{image:{url: `https://telegra.ph/file/7e1f9a45d326abc34d161.jpg`},caption:dec},{quoted:mek});
+await conn.sendMessage(from,{image:{url: `https://telegra.ph/file/f5e0751267307ac475187.mp4`},caption:dec},{quoted:mek});
 
 }catch(e){
 console.log(e)
