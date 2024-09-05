530c063914363e99408aa15f9bd03ae3decdb1a4
Nadeenpoorna-max <nadeenpoorna5@gmail.com>
2024-09-05 10:22:39 +0530

    Update song+video.js

--- plugins/song+video.js
+++ plugins/song+video.js
@@ -1,89 +1,106 @@
 const {cmd , commands} = require('../command')
 const fg = require('api-dylux')
 const yts = require('yt-search')
-
-
 cmd({
     pattern: "song",
-    desc: "download song.",
+    desc: "To download songs.",
+    react: "🎵",
     category: "download",
-    reaction: "🎵"
     filename: __filename
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
-if(!q) return reply("Please Give me `URL` or `TITLE`")
+if(!q) return reply("Please give me a url or title")  
 const search = await yts(q)
-const data = search.videos[0]:
+const data = search.videos[0];
 const url = data.url
+    
+    
+let desc = `
+⫷⦁[ * '-'_꩜ 𝐃𝐀𝐑𝐊 𝐙𝐄𝐑𝐎 𝐌𝐃 𝐒𝐎𝐍𝐆 𝐃𝐎𝐖𝐍𝐋𝐎𝐀𝐃𝐄𝐑 ꩜_'-' * ]⦁⫸
 
-let desc = '
-⚡ `NADEEN-MD` SONG DOWNLOADER ⚡
+🎵 *Song Found!* 
 
-title: ${data.title}
-description: ${data.description}
-time: ${data.timestamp}
-ago: ${data.ago}
-views: ${data.views}
+➥ *Title:* ${data.title} 
+➥ *Duration:* ${data.timestamp} 
+➥ *Views:* ${data.views} 
+➥ *Uploaded On:* ${data.ago} 
+➥ *Link:* ${data.url} 
 
-MADE BY `NADEEN POORNA`     
-'
-await conn.sendMessage(from,{image:{url}: data.thumbnail},caption:desc},{quoted:mek});
+🎧 *Enjoy the music brought to you by* *NADEEN-MD*! 
+
+> *MADE💛by NADEEN POORNA* 
+ 
+> *👨‍💻 ɴᴀᴅᴇᴇɴ-ᴍᴅ ᴍᴀᴅᴇ ʙʏ ɴᴀᴅᴇᴇɴ ᴘᴏᴏʀɴᴀ 👨‍💻* 
+*💻 GitHub:* https://github.com/Nadeenpoorna-max/NADEEN-MD  
+`
+
+await conn.sendMessage(from,{image:{url: data.thumbnail},caption:desc},{quoted:mek});
 
 //download audio
 
 let down = await fg.yta(url)
 let downloadUrl = down.dl_url
 
-//send audio + document messege
-await conn.sendMessage(from,{audio: {url:downloadUrl,mimetype:"audio/mpeg"},{quoted:mek})
-await conn.sendMessage(from,{document: {url:downloadUrl,mimetype:"audio/mpeg",fileName:data.title + ".mp3",caption:"`ɴᴀᴅᴇᴇɴ-ᴍᴅ` ᴍᴀᴅᴇ ʙʏ ɴᴀᴅᴇᴇɴ ᴘᴏᴏʀɴᴀ"},{quoted:mek})
-    
+//send audio message
+await conn.sendMessage(from,{audio: {url:downloadUrl},mimetype:"audio/mpeg"},{quoted:mek})
+await conn.sendMessage(from,{document: {url:downloadUrl},mimetype:"audio/mpeg",fileName:data.title + ".mp3",caption:"*©👨‍💻 ɴᴀᴅᴇᴇɴ-ᴍᴅ ᴍᴀᴅᴇ ʙʏ ɴᴀᴅᴇᴇɴ ᴘᴏᴏʀɴᴀ 👨‍💻*"},{quoted:mek})
+
 }catch(e){
-  console.log(e)
-  reply(`${e}`) 
+console.log(e)
+  reply('𝐶𝑎𝑛𝑡 𝐹𝑖𝑛𝑑 α ѕσηg')
 }
 })
 
-//===========video-dl=============
+//====================video_dl=======================
+
 cmd({
     pattern: "video",
-    desc: "download videos.",
+    desc: "To download videos.",
+    react: "🎥",
     category: "download",
-     reaction: "🎥"
     filename: __filename
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
-if(!q) return reply("Please Give me `URL` or `TITLE`")
+if(!q) return reply("Please give me a url or title")  
 const search = await yts(q)
-const data = search.videos[0]:
+const data = search.videos[0];
 const url = data.url
+    
+    
+let desc = `
+⫷⦁[ * '-'_꩜ 𝘿𝘼𝙍𝙆 𝙕𝙀𝙍𝙊 𝙈𝘿 𝘿𝙊𝙒𝙉𝙇𝙊𝘼𝘿𝙀𝙍 ꩜_'-' * ]⦁⫸ 
 
-let desc = '
-⚡ `NADEEN-MD` VIDEO DOWNLOADER ⚡
+🎥 *Video Found!* 
 
-title: ${data.title}
-description: ${data.description}
-time: ${data.timestamp}
-ago: ${data.ago}
-views: ${data.views}
+➥ *Title:* ${data.title} 
+➥ *Duration:* ${data.timestamp} 
+➥ *Views:* ${data.views} 
+➥ *Uploaded On:* ${data.ago} 
+➥ *Link:* ${data.url} 
 
-MADE BY `NADEEN POORNA`     
-'
-await conn.sendMessage(from,{image:{url}: data.thumbnail},caption:desc},{quoted:mek});
+🎬 *Enjoy the video brought to you by* *NADEEN-MD*! 
+
+> *MADE 🍀 by NADEEN POOORNA* 
+
+> *👨‍💻 ɴᴀᴅᴇᴇɴ-ᴍᴅ ᴍᴀᴅᴇ ʙʏ ɴᴀᴅᴇᴇɴ ᴘᴏᴏʀɴᴀ 👨‍💻* 
+*💻 GitHub:* https://github.com/Nadeenpoorna-max/NADEEN-MD
+`
+
+await conn.sendMessage(from,{image:{url: data.thumbnail},caption:desc},{quoted:mek});
 
 //download video
 
 let down = await fg.ytv(url)
 let downloadUrl = down.dl_url
 
-//send video + document messege
-await conn.sendMessage(from,{video: {url:downloadUrl,mimetype:"video/mp4"},{quoted:mek})
-await conn.sendMessage(from,{document: {url:downloadUrl,mimetype:"video/mp4",fileName:data.title + ".mp4",caption:"`ɴᴀᴅᴇᴇɴ-ᴍᴅ` ᴍᴀᴅᴇ ʙʏ ɴᴀᴅᴇᴇɴ ᴘᴏᴏʀɴᴀ"},{quoted:mek})
-    
+//send video message
+await conn.sendMessage(from,{video: {url:downloadUrl},mimetype:"video/mp4"},{quoted:mek})
+await conn.sendMessage(from,{document: {url:downloadUrl},mimetype:"video/mp4",fileName:data.title + ".mp4",caption:"*©👨‍💻 ɴᴀᴅᴇᴇɴ-ᴍᴅ ᴍᴀᴅᴇ ʙʏ ɴᴀᴅᴇᴇɴ ᴘᴏᴏʀɴᴀ 👨‍💻*"},{quoted:mek})
+
 }catch(e){
-  console.log(e)
-  reply(`${e}`) 
+console.log(e)
+  reply('𝐶𝑎𝑛𝑡 𝐹𝑖𝑛𝑑 α νι∂єσ')
 }
 })
