98b3fa7ce6d0c50139fe7fe8c1f7570112217b1c
Nadeenpoorna-max <nadeenpoorna5@gmail.com>
2024-10-06 10:31:36 +0530

    👨‍💻 NADEEN-MD 👨‍💻

--- config.js
+++ config.js
@@ -5,10 +5,10 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "put your session id",
+SESSION_ID: process.env.SESSION_ID || "", //put your session id
 ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/c7ce95554df8fcfa85680.jpg",
 ALIVE_MSG: process.env.ALIVE_MSG || "*👋 Hello ${pushname}* ✫💫𝐍𝐀𝐃𝐄𝐄𝐍-𝐌𝐃💫✫\n\n*Hey there!*\n\n 💫 *ηα∂єєη-м∂ ωнαтѕαρρ вσт ιѕ υρ αη∂ яυηηιηg!*\n🖤 *яυηтιмє : 1 hour, 5 minutes, 44 seconds\n👨‍💻 *¢яєαтє∂ ву:* NADEEN POORNA\n\n*𝐻𝑒𝓇𝑒'𝓈 𝓌𝒽𝒶𝓉 𝐼 𝒸𝒶𝓃 𝒹𝑜*\n💿 *Download Songs & Videos*\n📰 *Fetch Latest News*\n🎭 *Entertain with Fun Commands*\n🔧 *Manage Groups*\n\n*𝒮𝓉𝒶𝓎 𝒸𝑜𝓃𝓃𝑒𝒸𝓉𝑒𝒹 𝒶𝓃𝒹 𝑒𝓃𝒿𝑜𝓎 𝓉𝒽𝑒 𝓈𝑒𝓇𝓋𝒾𝒸𝑒𝓈!*\n\n💕𝙃𝙚𝙡𝙥𝙚𝙧𝙨💕: \n MANHIRU YUWAN\n> INDUWARA DUSHRSHANGANA\n> DINKA\n\n_*©𝐍𝐀𝐃𝐄𝐄𝐍-𝐌𝐃*_\n*👨‍💻 ɴᴀᴅᴇᴇɴ-ᴍᴅ ᴍᴀᴅᴇ ʙʏ ɴᴀᴅᴇᴇɴ ᴘᴏᴏʀɴᴀ 👨‍💻*",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
-MODE: process.env.MODE || "public",
-AUTO_VOICE:"true"
+MODE: process.env.MODE || "public", //private or public
+AUTO_VOICE:"true" //true or false
 };
