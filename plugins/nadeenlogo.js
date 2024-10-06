dbbff4a1a2991503b08783a5cc442cebe3dfdfe4
Nadeenpoorna-max <nadeenpoorna5@gmail.com>
2024-10-06 17:37:20 +0530

    Update nadeenlogo.js

--- plugins/nadeenlogo.js
+++ plugins/nadeenlogo.js
@@ -15,7 +15,7 @@ let dec = `❮❮ NADEEN-MD WALLPEPAR 👾💗 ❯❯`
 await conn.sendMessage(from,{image:{url: `https://telegra.ph/file/c7ce95554df8fcfa85680.jpg`},caption:dec},{quoted:mek});
 await conn.sendMessage(from,{image:{url: `https://telegra.ph/file/c253ba5258dc2c5c2f33d.png`},caption:dec},{quoted:mek});
 await conn.sendMessage(from,{image:{url: `https://telegra.ph/file/f2a6ee1ab649a2fb72708.jpg`},caption:dec},{quoted:mek});
-await conn.sendMessage(from,{image:{url: `https://www.imgtr.net/ib/7ghhUAnAw1MgXnU_1728205341.png`},caption:dec},{quoted:mek});
+await conn.sendMessage(from,{image:{url: `https://i.imgur.com/AzQrKon.png`},caption:dec},{quoted:mek});
 
 }catch(e){
 console.log(e)
