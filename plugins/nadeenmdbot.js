ad965945e1a8422741a9cb2d1293a35d441bd426
Nadeenpoorna-max <nadeenpoorna5@gmail.com>
2024-10-06 17:47:30 +0530

    Update nadeenmdbot.js

--- plugins/nadeenmdbot.js
+++ plugins/nadeenmdbot.js
@@ -34,7 +34,7 @@ let about = ` *👋 Hello ${pushname}*
 
 *👨‍💻ɴᴀᴅᴇᴇɴ-ᴍᴅ ᴍᴀᴅᴇ ʙʏ ɴᴀᴅᴇᴇɴ ᴘᴏᴏʀɴᴀ👨‍💻*`
 
-return await conn.sendMessage(from,{image: {url:`https://www.imgtr.net/ib/7ghhUAnAw1MgXnU_1728205341.png`},caption:about},{quoted: mek})
+return await conn.sendMessage(from,{image: {url:`https://i.imgur.com/rRdKMrE.png`},caption:about},{quoted: mek})
 
 }catch(e){
 
