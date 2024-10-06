59a2e5fe1c981b9bc59a238188cee189a97ee9f1
Nadeenpoorna-max <nadeenpoorna5@gmail.com>
2024-10-06 18:08:20 +0530

    Update setautobio.js

--- plugins/setautobio.js
+++ plugins/setautobio.js
@@ -10,7 +10,7 @@ cmd({
     pattern: "setautobio",
     desc: "Enable or disable the AutoBIO feature.",
     category: "owner",
-    react: "👨🏻‍💻",
+    react: "🍀",
     filename: __filename
 }, async (conn, mek, m, { from, isOwner, reply }) => {
     if (!isOwner) return reply("❌ You are not the owner!");
@@ -34,7 +34,7 @@ function startAutoBio(conn) {
     // Set a new interval to update the bio every minute (or any preferred time)
     autoBioInterval = setInterval(async () => {
         const time = new Date().toLocaleTimeString();  // Get the current time
-        const bioText = `💃 NADEEN-MD [${time}] 👨‍💻`;  // Set the bio text with time
+        const bioText = `🤩 I am using NADEEN-MD [${time}] 👨‍💻`;  // Set the bio text with time
         await conn.updateProfileStatus(bioText);  // Update the bot's bio
     }, 60 * 1000);  // 1 minute interval
 }
