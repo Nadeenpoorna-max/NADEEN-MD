e0eb31177c5868ec6ec593dce3c6c8c4955b63f0
Thiwankanimsara <thiwankathiwanka519@gmail.com>
2024-10-07 21:50:45 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "", //put your session id
+SESSION_ID: process.env.SESSION_ID || "VCMEUBqA#Db13_ZrlfByQhfHR6A8uDfb-rWGjT0Y_PjZAKo7Gh14", //put your session id
 ALIVE_IMG: process.env.ALIVE_IMG || "https://www.imgtr.net/ib/7ghhUAnAw1MgXnU_1728205341.png",
 ALIVE_MSG: process.env.ALIVE_MSG || "*👋 Hello ${pushname}* ✫💫𝐍𝐀𝐃𝐄𝐄𝐍-𝐌𝐃💫✫\n\n*Hey there!*\n\n 💫 *ηα∂єєη-м∂ ωнαтѕαρρ вσт ιѕ υρ αη∂ яυηηιηg!*\n🖤 *яυηтιмє : 1 hour, 5 minutes, 44 seconds\n👨‍💻 *¢яєαтє∂ ву:* NADEEN POORNA\n\n*𝐻𝑒𝓇𝑒'𝓈 𝓌𝒽𝒶𝓉 𝐼 𝒸𝒶𝓃 𝒹𝑜*\n💿 *Download Songs & Videos*\n📰 *Fetch Latest News*\n🎭 *Entertain with Fun Commands*\n🔧 *Manage Groups*\n\n*𝒮𝓉𝒶𝓎 𝒸𝑜𝓃𝓃𝑒𝒸𝓉𝑒𝒹 𝒶𝓃𝒹 𝑒𝓃𝒿𝑜𝓎 𝓉𝒽𝑒 𝓈𝑒𝓇𝓋𝒾𝒸𝑒𝓈!*\n\n💕𝙃𝙚𝙡𝙥𝙚𝙧𝙨💕: \n MANHIRU YUWAN\n> INDUWARA DUSHRSHANGANA\n> DINKA\n\n_*©𝐍𝐀𝐃𝐄𝐄𝐍-𝐌𝐃*_\n*👨‍💻 ɴᴀᴅᴇᴇɴ-ᴍᴅ ᴍᴀᴅᴇ ʙʏ ɴᴀᴅᴇᴇɴ ᴘᴏᴏʀɴᴀ 👨‍💻*",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
