6c4c754a848dab420928da537425339eb436f3a8
Nadeenpoorna-max <nadeenpoorna5@gmail.com>
2024-10-17 15:40:28 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,10 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "put your session id", //ඔයාගේ whatsapp එකට ආපු code එක මෙතනට දාන්න.
-MONGODB: process.env.MONGODB || "mongodb+srv://kulathungaasitha319:yjHB0DvFfStNfwPS@cluster0.3oijd.mongodb.net/",
-ALIVE_IMG: process.env.ALIVE_IMG || "https://i.imgur.com/AzQrKon.png"
-}
+SESSION_ID: process.env.SESSION_ID || "", //put your session id
+ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/l7e7kt.png",
+ALIVE_MSG: process.env.ALIVE_MSG || "*👋 Hello ${pushname}* ✫💫𝐍𝐀𝐃𝐄𝐄𝐍-𝐌𝐃💫✫\n\n*Hey there!*\n\n 💫 *ηα∂єєη-м∂ ωнαтѕαρρ вσт ιѕ υρ αη∂ яυηηιηg!*\n🖤 *яυηтιмє : 1 hour, 5 minutes, 44 seconds\n👨‍💻 *¢яєαтє∂ ву:* NADEEN POORNA\n\n*𝐻𝑒𝓇𝑒'𝓈 𝓌𝒽𝒶𝓉 𝐼 𝒸𝒶𝓃 𝒹𝑜*\n💿 *Download Songs & Videos*\n📰 *Fetch Latest News*\n🎭 *Entertain with Fun Commands*\n🔧 *Manage Groups*\n\n*𝒮𝓉𝒶𝓎 𝒸𝑜𝓃𝓃𝑒𝒸𝓉𝑒𝒹 𝒶𝓃𝒹 𝑒𝓃𝒿𝑜𝓎 𝓉𝒽𝑒 𝓈𝑒𝓇𝓋𝒾𝒸𝑒𝓈!*\n\n💕𝙃𝙚𝙡𝙥𝙚𝙧𝙨💕: \n MANHIRU YUWAN\n> INDUWARA DUSHRSHANGANA\n> DINKA\n\n_*©𝐍𝐀𝐃𝐄𝐄𝐍-𝐌𝐃*_\n*👨‍💻 ɴᴀᴅᴇᴇɴ-ᴍᴅ ᴍᴀᴅᴇ ʙʏ ɴᴀᴅᴇᴇɴ ᴘᴏᴏʀɴᴀ 👨‍💻*",
+AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
+MODE: process.env.MODE || "public", //private or public
+AUTO_VOICE:"true" //true or false
+};
