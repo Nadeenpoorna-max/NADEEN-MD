16c7d93c03f33c03e5cdadfdbd8c957af2fa7042
Nadeenpoorna-max <nadeenpoorna5@gmail.com>
2024-10-17 14:08:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ Browsers
 
 const { getBuffer, getGroupAdmins, getRandom, h2k, isUrl, Json, runtime, sleep, fetchJson } = require('./lib/functions')
 const fs = require('fs')
+const l = console.log
 const P = require('pino')
 const config = require('./config')
 const qrcode = require('qrcode-terminal')
