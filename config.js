ee7081f501f516ee50ae92bcfdcbf3b01e72f2ff
Nadeenpoorna-max <nadeenpoorna5@gmail.com>
2024-10-17 13:37:05 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "NHcDwITD#guDIpaLiZcgArJkmFm-wS3_-zoWN9lPW6hQ3rsrs0as",
+SESSION_ID: process.env.SESSION_ID || "put your session id", //ඔයාගේ whatsapp එකට ආපු code එක මෙතනට දාන්න.
 MONGODB: process.env.MONGODB || "mongodb+srv://kulathungaasitha319:yjHB0DvFfStNfwPS@cluster0.3oijd.mongodb.net/",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://i.imgur.com/AzQrKon.png"
 }
